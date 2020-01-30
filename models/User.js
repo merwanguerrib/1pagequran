4f8da8f5829db3e18eda79fc8502a5a058a6a6f3
Merwan Guerrib <merwanguerrib@gmail.com>
2020-01-30 15:14:34 +0100

    Add User model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    email: String,
+    advancement: Number
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
