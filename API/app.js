e64790e3dfff1a1c19994cf3ce50f6c6419890a3
Merwan Guerrib <merwanguerrib@gmail.com>
2020-10-15 21:14:06 +0200

    Refactorize app.js with the new createMail function

--- API/app.js
+++ API/app.js
@@ -5,10 +5,7 @@ const app = express();
 
 const mongooseConnection = require("./services/mongoose/mongooseConnection");
 
-const getRecipients = require("./services/getRecipients");
-const createPage = require("./services/createPage");
-const sendMail = require("./services/sendMail");
-const incrementAdvancement = require("./services/incrementAdvancement");
+const createMail = require("./services/createMail");
 
 const cron = require("node-cron");
 
@@ -30,79 +27,13 @@ mongooseConnection();
 const signup = require("./routes/signup");
 app.use("/", signup);
 
-const main = async () => {
-  // Create recipients array
-  const recipients = await getRecipients();
-
-  // Iterate through recipients array
-  recipients.map(async (recipient) => {
-    // Set the page
-    let pageObjectToRender = {
-      srcUrl: "",
-      verses: [],
-    };
-    // Set the options of the email
-    let options = {
-      method: "POST",
-      url: "https://api.sendgrid.com/v3/mail/send",
-      headers: {
-        "content-type": "application/json",
-        authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-      },
-      body: {
-        personalizations: [
-          {
-            to: [{ email: recipient.email }],
-            dynamic_template_data: {
-              pageObjectToRender: pageObjectToRender,
-              subject:
-                recipient.translationType === `en`
-                  ? `Read the page ${recipient.advancement} today !`
-                  : recipient.translationType === `fr`
-                  ? `Lis la page ${recipient.advancement} aujourd'hui`
-                  : null,
-            },
-          },
-        ],
-        from: { email: "pageoftheday@1pagequran.com", name: "Merwan" },
-        reply_to: { email: "pageoftheday@1pagequran.com", name: "Merwan" },
-        template_id: `${process.env.Template_ID}`,
-      },
-      json: true,
-    };
-
-    await createPage(recipient)
-      .then((res) => {
-        options.body.personalizations[0].dynamic_template_data.pageObjectToRender = res;
-        console.log(
-          `recipient.email : ${recipient.email}`,
-          `dynamic_template_data : ${JSON.stringify(
-            options.body.personalizations[0].dynamic_template_data
-              .pageObjectToRender
-          )}`
-        );
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-
-    try {
-      sendMail(options);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      await incrementAdvancement(recipient);
-    }
-  });
-};
-
 // Function Call to be deleted after testing
-// main();
+createMail();
 
 // ########## UNCOMMENT AFTER TESTING ########
 //Setup the Cron to execute everyday at 7:00 am (change to '* * * * *' to execute every minute for test purposes)
 // cron.schedule("0 7 * * *", () => {
-//   main();
+//   createMail();
 // });
 
 module.exports = app;
