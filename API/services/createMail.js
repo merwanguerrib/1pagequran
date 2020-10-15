a4ca3f994b459a18224a4586c3019dbe540da375
Merwan Guerrib <merwanguerrib@gmail.com>
2020-10-15 21:13:30 +0200

    Refactorize adding a createMail function in a separate file

---
+++ API/services/createMail.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+
+const getRecipients = require("./getRecipients");
+const createPage = require("./createPage");
+const sendMail = require("./sendMail");
+const incrementAdvancement = require("./incrementAdvancement");
+
+const createMail = async () => {
+  // Create recipients array
+  const recipients = await getRecipients();
+
+  // Iterate through recipients array
+  recipients.map(async (recipient) => {
+    // Set the page
+    let pageObjectToRender = {
+      srcUrl: "",
+      verses: [],
+    };
+    // Set the options of the email
+    let options = {
+      method: "POST",
+      url: process.env.SENGRID_MAIL_SEND_ENDPOINT,
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+      },
+      body: {
+        personalizations: [
+          {
+            to: [{ email: recipient.email }],
+            dynamic_template_data: {
+              pageObjectToRender: pageObjectToRender,
+              subject:
+                recipient.translationType === `en`
+                  ? `Read the page ${recipient.advancement} today !`
+                  : recipient.translationType === `fr`
+                  ? `Lis la page ${recipient.advancement} aujourd'hui`
+                  : null,
+            },
+          },
+        ],
+        from: { email: "pageoftheday@1pagequran.com", name: "Merwan" },
+        reply_to: { email: "pageoftheday@1pagequran.com", name: "Merwan" },
+        template_id: `${process.env.Template_ID}`,
+      },
+      json: true,
+    };
+
+    await createPage(recipient)
+      .then((res) => {
+        options.body.personalizations[0].dynamic_template_data.pageObjectToRender = res;
+        console.log(
+          `recipient.email : ${recipient.email}`,
+          `dynamic_template_data : ${JSON.stringify(
+            options.body.personalizations[0].dynamic_template_data
+              .pageObjectToRender
+          )}`
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    try {
+      sendMail(options);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await incrementAdvancement(recipient);
+    }
+  });
+};
+
+module.exports = createMail;
