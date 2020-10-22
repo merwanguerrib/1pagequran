f9a9b5e9e12e5fff7ff5c8839344172041527d16
Merwan Guerrib <merwanguerrib@gmail.com>
2020-10-22 16:05:47 +0200

    Update incrementAdvancement function with reinitializeAdvancement function call

--- API/services/incrementAdvancement.js
+++ API/services/incrementAdvancement.js
@@ -1,12 +1,16 @@
 const User = require("../models/User");
+const reinitializeAdvancement = require("./reinitializeAdvancement");
 
-const incrementAdvancement = async recipient => {
+const incrementAdvancement = async (recipient) => {
   const filter = { email: recipient.email };
   const update = { advancement: recipient.advancement + 1 };
-
-  let user = await User.findOneAndUpdate(filter, update, {
-    new: true
-  });
-  await user.save();
+  if (recipient.advancement === 604) {
+    reinitializeAdvancement(recipient);
+  } else {
+    let user = await User.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    await user.save();
+  }
 };
 module.exports = incrementAdvancement;
