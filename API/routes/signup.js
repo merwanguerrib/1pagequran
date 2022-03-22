4d0b3abc5ef6fe65818a948eb910e318660b8740
Merwan Guerrib <merwan.guerrib@capgemini.com>
2022-03-22 20:10:46 +0100

    Add the verification email to the signup function

--- API/routes/signup.js
+++ API/routes/signup.js
@@ -1,25 +1,75 @@
 const express = require("express");
+
 const router = express.Router();
+
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
 const User = require("../models/User");
+const Token = require("../models/Token");
 
 // Adding a User
 router.post("/signup", (req, res) => {
   const email = req.body.email;
   const translationType = req.body.translation;
-  console.log(req.body);
-  const newUser = new User({
-    email: email,
-    advancement: 1,
-    translationType: translationType,
+  console.table(req.body);
+  User.findOne({ email: email }, function (err, newUser) {
+    if (err) {
+      return res.status(500).send({ msg: err.message }, "FindOne Error");
+    } else if (newUser) {
+      return res.status(400).send({
+        msg: `This email address: ${req.body.email} is already associated with another account`,
+      });
+    } else {
+      newUser = new User({
+        email: email,
+        advancement: 1,
+        translationType: translationType,
+      });
+      newUser.save(function (err) {
+        if (err) {
+          return res.status(500).send({ msg: err.message });
+        }
+        let token = new Token({
+          _userId: newUser._id,
+          token: crypto.randomBytes(16).toString("hex"),
+        });
+        token.save(function (err) {
+          if (err) {
+            return res.status(500).send({ msg: err.message });
+          }
+          const transporter = nodemailer.createTransport(
+            sendgridTransport({
+              auth: {
+                api_key: process.env.SENDGRID_API_KEY,
+              },
+            })
+          );
+          let mailOptions = {
+            from: "1pagequran.com",
+            to: newUser.email,
+            subject: "Confirm your email adress",
+            text: `Hello, please verify your email adress : ${newUser.email}\n 
+              Click the following link : \n
+              https:\\${req.headers.host}\/confirmation\/${newUser.email}\/${token.token}
+              \n\nThank You!\n
+              `,
+          };
+          transporter.sendMail(mailOptions, function (err) {
+            if (err) {
+              return res.status(500).send({ msg: err.message });
+            }
+            return res
+              .status(200)
+              .send(
+                `A verification email has been sent to ${newUser.email}. It will be expire after one day. If you did not get verification email click on resend token.`
+              );
+          });
+        });
+      });
+    }
   });
-  newUser
-    .save()
-    .then((user) => {
-      res.send(user);
-    })
-    .catch((err) => {
-      console.log(error);
-    });
 });
 
 module.exports = router;
