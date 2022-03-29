1ac9b7dece4840b7b6e37e41d76d1b945aef040d
Merwan Guerrib <merwan.guerrib@capgemini.com>
2022-03-29 16:44:55 +0200

    Reuse sendMail.js Method to send the email address confirmation email

--- API/services/sendConfirmationEmail.js
+++ API/services/sendConfirmationEmail.js
@@ -1,35 +1,45 @@
-const nodemailer = require("nodemailer");
-const sendgridTransport = require("nodemailer-sendgrid-transport");
+require("dotenv").config();
+
+const sendMail = require("./sendMail");
+
 const { ErrorHandler } = require("../helpers/error");
 
 const sendConfirmationEmail = async (req, res, next, newUser, token) => {
-  const transporter = nodemailer.createTransport(
-    sendgridTransport({
-      auth: {
-        api_key: process.env.SENDGRID_API_KEY,
-      },
-    })
-  );
-  let mailOptions = {
-    from: "pageoftheday@1pagequran.com",
-    to: newUser.email,
-    subject: "Please confirm your email adress",
-    html: `
-    Hello, please verify your email adress : ${newUser.email}
-    Click the following link : ${process.env.PUBLIC_URL}\/confirmation\/${token.token}
-    Thank You!`,
+  // Set the options of the email
+  let options = {
+    method: "POST",
+    url: process.env.SENGRID_MAIL_SEND_ENDPOINT,
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+    },
+    body: {
+      personalizations: [
+        {
+          to: [{ email: newUser.email }],
+          dynamic_template_data: {
+            confirmationLink: `${process.env.PUBLIC_URL}\/confirmation\/${token.token}`,
+            subject:
+              newUser.translationType === `en`
+                ? `Please confirm your email address`
+                : newUser.translationType === `fr`
+                ? `Merci de confirmer votre adresse email`
+                : null,
+          },
+        },
+      ],
+      from: { email: "pageoftheday@1pagequran.com", name: "1PageQuran" },
+      reply_to: { email: "no-reply@1pagequran.com", name: "1PageQuran" },
+      template_id: `${process.env.EmailConfirmationTemplate_ID}`,
+    },
+    json: true,
   };
-  transporter.sendMail(mailOptions, function (error) {
-    if (error) {
-      throw new ErrorHandler(error.statusCode, error.message);
-    }
-    console.log("mailOptions==>", mailOptions);
-    return res.status(200).send(
-      `A verification email has been sent to ${newUser.email}.
-       It will expire after in 24h.
-       If you did not get the verification email click on resend token.`
-    );
-  });
+  try {
+    sendMail(options);
+  } catch (error) {
+    console.log("CreateToken catch error");
+    throw new ErrorHandler(error.statusCode, error.message);
+  }
 };
 
 module.exports = sendConfirmationEmail;
